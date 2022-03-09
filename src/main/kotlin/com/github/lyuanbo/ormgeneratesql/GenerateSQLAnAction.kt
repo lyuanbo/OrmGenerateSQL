21741e6d02a621e1d1a462ebcdf2409e6844da27
刘远波 <liuyuanbo@ydjdev.com>
2022-03-09 18:15:09 +0800

    fix: 增加action,ui

---
+++ src/main/kotlin/com/github/lyuanbo/ormgeneratesql/GenerateSQLAnAction.kt
@@ -0,0 +1,15 @@
+package com.github.lyuanbo.ormgeneratesql
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GenerateSQLAnAction: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // 数据库配置
+
+
+
+      println(e)
+    }
+
+}
