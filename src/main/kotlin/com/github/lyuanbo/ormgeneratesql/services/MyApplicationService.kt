e377290397aab7b41baab56333fb8f0fce631c03
GitHub Action <action@github.com>
2022-03-09 08:15:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/lyuanbo/ormgeneratesql/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lyuanbo/ormgeneratesql/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lyuanbo.ormgeneratesql.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lyuanbo.ormgeneratesql.MyBundle
 
 class MyApplicationService {
 
